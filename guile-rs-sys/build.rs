c4a761ad06560d9086ed6c51a141f35b0486fe4c
Sheheryar Parvaz <skipper308@hotmail.ca>
2018-01-20 23:00:21 -0500

    Improve build script for portability
    gcc crate has been changed in favour of the cc crate. Include dirs now
    include /usr/local/include.

--- guile-rs-sys/build.rs
+++ guile-rs-sys/build.rs
@@ -1,5 +1,5 @@
 extern crate bindgen;
-extern crate gcc;
+extern crate cc;
 
 use std::env;
 use std::path::PathBuf;
@@ -8,15 +8,22 @@ fn main() {
     let manifest_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let out_path      = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    let guile_inc_dir = "/usr/include/guile/2.2/";
+    let include_dirs = [
+        "/usr/include/guile/2.2",
+        "/usr/local/include/guile/2.2",
+    ];
 
-    gcc::Build::new()
-        .include(guile_inc_dir)
+    let mut cc = cc::Build::new();
+
+    include_dirs
+        .iter()
+        .for_each(|s| { cc.include(s); }); // {} required to return ()
+
+    cc
         .file(manifest_path.join("wrapper.c"))
         .compile("guile_wrapper");
 
     // println!("cargo:rustc-link-lib=static=guile_wrapper");
-
 
     // println!("cargo:include=/usr/include/guile/2.2/");
     println!("cargo:rustc-link-lib=guile-2.2");
@@ -25,13 +32,14 @@ fn main() {
         .header(manifest_path.join("wrapper.c").to_str().unwrap())
         .ctypes_prefix("libc")
 
-        .clang_args(vec![format!("-I{}", guile_inc_dir)])
+        .clang_args(include_dirs // Add each directory
+                        .into_iter()
+                        .map(|s| format!("-I{}", s))
+                   )
         .generate()
         .expect("Unable to generate bindings");
 
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
-
 }
